7abfefef6d458710acf0d1841674cfb8a50a54f2
Revymarcy <ITITIU20152@student.hcmiu.edu.vn>
2024-06-14 19:18:36 +0700

    wrong folders

--- webproject--development/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost','root','','newstyle');
-if(!$conn){
-    header("Location: ../error/500.php");
-    exit('Database connection failed !!!');
-}
-?>
--- webproject--development/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('location: ./pages/home.php');
-?>
